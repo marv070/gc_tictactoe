89b0eda143d181164bd36f6fa3075d3707d538a3
Marvin Laucher <marv070@gmail.com>
2015-08-29 11:54:11 -0400

    set player markers

--- main.rb
+++ main.rb
@@ -16,3 +16,35 @@ puts lines
 end 
  new_board("Welcome to Tic Tac Toe")
  new_board("The current board is")
+ 
+def define_marker
+ puts "Player 1 do you want x or o"
+ player1 = gets.chomp
+end
+
+
+def player2_marker(p1_marker)
+  if p1_marker == "x"
+   "o"
+  else "x"
+  end
+end
+
+
+def get_player_marker
+  player1 = define_marker
+  player2 = player2_marker(player1)
+  puts "Player1 is #{player1} so player2 is #{player2}."
+end
+
+
+get_player_marker
+
+
+
+
+
+
+
+
+
