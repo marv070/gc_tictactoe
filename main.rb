df4e716e910121e8e5366c8a458e62e20cc5fdeb
Marvin Laucher <marvinlaucher@Marvins-MacBook-Pro.local>
2015-08-29 10:07:18 -0400

    Initial commit

---
+++ main.rb
@@ -0,0 +1,17 @@
+def new_board(lines)
+
+
+
+  puts """
+
+   |   | 
+ 1 | 2 | 3
+---|---|---
+   |   |
+ 4 | 5 | 6
+---|---|---
+   |   | 
+ 7 | 8 | 9
+ """
+end 
+ new_board("This is the new board")
