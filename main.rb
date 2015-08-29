0465f9c25aad477f6a1d9c2eae82f4580b0a0dbc
Marvin Laucher <marv070@gmail.com>
2015-08-29 15:02:35 -0400

    update first player choice on board

--- main.rb
+++ main.rb
@@ -16,11 +16,10 @@ puts lines
  """
 end 
 board_position_array = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-board_empty = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
+current_board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
  new_board("Welcome to Tic Tac Toe", board_position_array)
- new_board("The current board is", board_empty )
+ new_board("The current board is", current_board )
  
 
 def define_marker
@@ -46,10 +45,12 @@ end
 
 set_player_marker
 
-#puts "Player 1 #{@player1}, please choose square 1 thru 9 for your first move."
+puts "Player 1 #{@player1}, please choose square 0 thru 8 for your first move."
 square = gets.chomp.to_i
-#puts " Player1 has choosen #{square}."
+puts " Player1 has choosen #{square}."
 
+current_board[square] = "#{@player1}"
+new_board("player (#{@player1}) chose space #{square}", current_board)
 
 
 
